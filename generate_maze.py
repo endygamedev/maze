ad70d5cf54f2143e7c778de0a26a449c543ca081
Egor <bronnikov.40@mail.ru>
2021-08-18 21:06:06 +0300

    Edit some GUI things and fixed bug with rectangular matrix

--- generate_maze.py
+++ generate_maze.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import random
 
 
@@ -7,60 +6,64 @@ class Maze:
         self.__width = width
         self.__height = height
 
-        self.start_x, self.start_y = random.randint(1, width//2), random.randint(1, height//2)
-        self.finish = (random.randint(width//2, width - 2), random.randint(height//2, height - 2))
+        self.start_x, self.start_y = random.randint(1, width - 2), random.randint(1, height - 2)
+        self.finish = (random.randint(1, width - 2), random.randint(1, height - 2))
 
-        self.__maze = [[1 for _ in range(width)] for _ in range(height)]
-        self.__maze[self.start_x][self.start_y] = 0
+        self._maze = [[1 for _ in range(width)] for _ in range(height)]
+        self._maze[self.start_y][self.start_x] = 0
 
         self.__current_x = self.start_x
         self.__current_y = self.start_y
-        self.__cells = [(self.__current_x, self.__current_y)]
+        self._cells = [(self.__current_x, self.__current_y)]
 
     def can_go(self, way):
         if way == "up":
-            return self.__current_x - 1 > 0 and self.__maze[self.__current_x - 1][self.__current_y] == 1
+            return self.__current_y - 1 > 0 and self._maze[self.__current_y - 1][self.__current_x] == 1
         elif way == "down":
-            return self.__current_x < len(self.__maze) - 2 and self.__maze[self.__current_x + 1][self.__current_y] == 1
+            return self.__current_y < self.__height - 2 and self._maze[self.__current_y + 1][self.__current_x] == 1
         elif way == "left":
-            return self.__current_y - 1 > 0 and self.__maze[self.__current_x][self.__current_y - 1] == 1
+            return self.__current_x - 1 > 0 and self._maze[self.__current_y][self.__current_x - 1] == 1
         else:
-            return self.__current_y < len(self.__maze[0]) - 2 and self.__maze[self.__current_x][self.__current_y + 1] == 1
+            return self.__current_x < self.__width - 2 and self._maze[self.__current_y][self.__current_x + 1] == 1
 
     def get_direction(self, way):
         if way == "up":
-            self.__current_x -= 1
-        elif way == "down":
-            self.__current_x += 1
-        elif way == "left":
             self.__current_y -= 1
-        else:
+        elif way == "down":
             self.__current_y += 1
-        self.__maze[self.__current_x][self.__current_y] = 0
+        elif way == "left":
+            self.__current_x -= 1
+        else:
+            self.__current_x += 1
+        self._maze[self.__current_y][self.__current_x] = 0
 
     def create_maze(self):
         while (self.__current_x, self.__current_y) != self.finish:
-            ncells = self.__cells[-(len(self.__cells)//(self.__width//2)):]
-            self.__current_x, self.__current_y = random.choice(ncells)
+            n = min(self.__width, self.__height)//2
+            n_cells = self._cells[-(len(self._cells)//n):]
+            self.__current_x, self.__current_y = random.choice(n_cells)
             ways = ["up", "down", "left", "right"]
             current_way = random.choice(ways)
 
             while not any([self.can_go(way) for way in ways]):
-                ncells = [(x, y) for (x, y) in ncells[-(len(ncells)//(self.__width//2)):] if x != self.__current_x and y != self.__current_y]
+                n_cells = [(x, y) for (x, y) in n_cells[-(len(n_cells)//n):] if x != self.__current_x and y != self.__current_y]
                 try:
-                    self.__current_x, self.__current_y = random.choice(ncells)
+                    self.__current_x, self.__current_y = random.choice(n_cells)
                 except IndexError:
-                    self.__current_x, self.__current_y = random.choice(self.__cells)
+                    self.__current_x, self.__current_y = random.choice(self._cells)
 
             while not self.can_go(current_way):
                 ways.remove(current_way)
                 current_way = random.choice(ways)
 
             self.get_direction(current_way)
-            self.__cells.append((self.__current_x, self.__current_y))
-        return self.__maze
+            self._cells.append((self.__current_x, self.__current_y))
+        return self._maze
 
 
 if __name__ == "__main__":
-    maze = Maze(width=200, height=100)
-    pprint(maze.create_maze())
+    from pprint import pprint
+
+    maze = Maze(width=20, height=10)
+    maze.create_maze()
+    pprint(maze._maze)
--- maze.py
+++ maze.py
@@ -1,40 +1,37 @@
 import tkinter as tk
-import time
 from PIL import Image, ImageTk, ImageDraw
 from generate_maze import Maze
 
 
 class App(tk.Tk):
-    def __init__(self, *args, maze=None, start=None, finish=None, zoom=1, **kwargs) -> None:
+    def __init__(self, *args, width=200, height=100, zoom=1, **kwargs) -> None:
         super(App, self).__init__(*args, **kwargs)
 
         self.title("Maze")
+        self.resizable(False, False)
+        self.geometry(f"{width * zoom + 100}x{height * zoom + 150}")
 
-        self.__maze = maze
-        self.__start = start
-        self.__finish = finish
+        self.__width = width
+        self.__height = height
         self.__zoom = zoom
 
-        self.__gui()
+        self.__start = None
+        self.__finish = None
+        self.__maze = None
 
+        self.__gui()
         self.mainloop()
 
-    # Iterative step
     def make_step(self, m, k):
         for i in range(len(m)):
             for j in range(len(m[0])):
-                # Edit last cells
                 if m[i][j] == k:
-                    # Left
                     if i > 0 and m[i - 1][j] == 0 and self.__maze[i - 1][j] == 0:
                         m[i - 1][j] = k + 1
-                    # Down
                     if j > 0 and m[i][j - 1] == 0 and self.__maze[i][j - 1] == 0:
                         m[i][j - 1] = k + 1
-                    # Right
                     if j < len(m[0]) - 1 and m[i][j + 1] == 0 and self.__maze[i][j + 1] == 0:
                         m[i][j + 1] = k + 1
-                    # Up
                     if i < len(m) - 1 and m[i + 1][j] == 0 and self.__maze[i + 1][j] == 0:
                         m[i + 1][j] = k + 1
         return m
@@ -45,9 +42,9 @@ class App(tk.Tk):
         width, height = img.size
         for x in range(0, width, self.__zoom):
             for y in range(0, height, self.__zoom):
-                if (x // self.__zoom, y // self.__zoom) == self.__start:
+                if (y // self.__zoom, x // self.__zoom) == self.__start:
                     r, g, b = (0, 255, 0)
-                elif (x // self.__zoom, y // self.__zoom) == self.__finish:
+                elif (y // self.__zoom, x // self.__zoom) == self.__finish:
                     r, g, b = (255, 0, 255)
                 elif (x // self.__zoom, y // self.__zoom) == elem or m[x // self.__zoom][y // self.__zoom] == "path":
                     r, g, b = (0, 255, 255)
@@ -65,21 +62,20 @@ class App(tk.Tk):
         img_tk = ImageTk.PhotoImage(img.transpose(Image.ROTATE_90).transpose(Image.FLIP_TOP_BOTTOM))
         self.__label.config(image=img_tk)
         self.__label.image = img_tk
-        time.sleep(0.01)
         self.update()
 
     def scan(self):
         m = [[0 for _ in range(len(self.__maze[0]))] for _ in range(len(self.__maze))]
-        m[self.__start[0]][self.__start[1]] = 1
+        m[self.__start[1]][self.__start[0]] = 1
         k = 0
-        while not m[self.__finish[0]][self.__finish[1]]:
+        while not m[self.__finish[1]][self.__finish[0]]:
             k += 1
             m = self.make_step(m, k)
             self.update_image(m)
         return m
 
     def find_path(self):
-        (x, y) = self.__finish
+        (y, x) = self.__finish
         m = self.scan()
         k = m[x][y]
         path = [(x, y)]
@@ -97,60 +93,37 @@ class App(tk.Tk):
         return path, m
 
     def btn_start_event(self):
+        self.__btn_generate["state"] = tk.DISABLED
+        self.__btn_start["state"] = tk.DISABLED
         path, m = self.find_path()
         for elem in path:
             self.update_image(m, elem)
+        self.__btn_generate["state"] = tk.NORMAL
+        self.__btn_start["state"] = tk.NORMAL
 
     def btn_generate_event(self):
-        mRand = Maze(width=50, height=50)
-        self.__start = (mRand.start_x, mRand.start_y)
-        self.__finish = mRand.finish
+        m_rand = Maze(width=self.__width, height=self.__height)
+        self.__start = (m_rand.start_x, m_rand.start_y)
+        self.__finish = m_rand.finish
 
-        self.__maze = mRand.create_maze()
+        self.__maze = m_rand.create_maze()
         self.update_image(self.__maze)
 
+        if self.__btn_start["state"] == tk.DISABLED:
+            self.__btn_start["state"] = tk.NORMAL
+
     def __gui(self):
-        btn_start = tk.Button(self, text="start", command=lambda: self.btn_start_event())
-        btn_generate = tk.Button(self, text="generate", command=lambda: self.btn_generate_event())
+        self.__btn_start = tk.Button(self, text="Find path", state=tk.DISABLED, command=lambda: self.btn_start_event())
+        self.__btn_generate = tk.Button(self, text="Generate maze", command=lambda: self.btn_generate_event())
+        btn_exit = tk.Button(self, text="Exit", command=self.quit)
         self.__label = tk.Label(self)
 
-        btn_start.pack(pady=10)
-        btn_generate.pack(pady=10)
-        self.__label.pack(fill=tk.BOTH, expand=True)
+        self.__btn_start.pack(pady=5)
+        self.__btn_generate.pack(pady=5)
+        self.__label.pack(fill=tk.BOTH, expand=True, pady=5)
+        btn_exit.pack(pady=5)
 
 
 if __name__ == "__main__":
-    m1 = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-
-    s1 = (1, 1)
-    f1 = (2, 5)
-
-    m2 = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0 ,0, 0, 0, 1, 0, 1, 1, 1, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0 ,0, 0, 0, 1, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 ,0, 0, 0, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
-
-    s2 = (1, 1)
-    f2 = (5, 19)
-
-    App(className="Maze", zoom=10)
+    App(className="Maze", width=90, height=120, zoom=5)
 
